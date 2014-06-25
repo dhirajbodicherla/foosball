3945a1bc8b1541128533afc6ad3b2fa7818de2e0
Dhiraj Bodicherla <dhiraj.bodicherla@gmail.com>
2014-06-24 21:04:59 -0400

    1. using orthographic camera 2. working on plane and importing texture to plane 3. fixed index.html (thank you sangha :D)

--- js/script.js
+++ js/script.js
@@ -1,36 +1,73 @@
 (function(){
 
-    var leftStick
+    var canvasWidth = window.innerWidth
+    , canvasHeight = window.innerHeight
+    , aspectRatio = canvasWidth / canvasHeight
+    , viewSize = 1000
+    , leftStick
     , rightStick
     , football
     , scene = new THREE.Scene()
-    , camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 2000)
+    , camera
     , renderer = new THREE.WebGLRenderer({ alpha: true })
     , clock = new THREE.Clock()
     , keyboard = new KeyboardState()
     , ambient = new THREE.AmbientLight( 0x101030 )
-    , directionalLight = new THREE.DirectionalLight( 0xffeedd );
+    , directionalLight = new THREE.DirectionalLight( 0xffeedd )
+    , ground;
 
     init();
 
     function init(){
 
+        //camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 2000)
+        //camera = new THREE.OrthographicCamera( window.innerWidth / 2, window.innerHeight / 2, 70, 1, 1000, - 1000, 1000 );
+        camera = new THREE.OrthographicCamera( -aspectRatio * viewSize / 2, aspectRatio * viewSize / 2, viewSize / 2, -viewSize / 2, -2000, 2000 );
+
+        camera.position.set(0, 100, 200);
+        //camera.rotation.x = Math.PI / 2;
+        camera.lookAt(new THREE.Vector3(0, 50, 0));
+
         renderer.setClearColor( 0x000000, 0 );
         renderer.setSize(window.innerWidth, window.innerHeight);
 
         document.body.appendChild(renderer.domElement);
-
-        camera.position.x = 2;
-        camera.position.y = 1;
-        camera.position.z = 6;
-
         
         scene.add( ambient );
-
         
         directionalLight.position.set( 2, 0, 1 );
         scene.add( directionalLight );
 
+        ground = new THREE.Mesh(new THREE.PlaneGeometry(canvasWidth, canvasHeight), new THREE.MeshNormalMaterial());
+        ground.position.set(0, -50, 0);
+        ground.rotation.x = Math.PI / 2;
+        
+        ground.overdraw = true;
+        scene.add(ground);
+
+      
+        // Grid
+        var size = 500, step = 50;
+
+        var geometry = new THREE.Geometry();
+
+        for ( var i = - size; i <= size; i += step ) {
+
+            geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
+            geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+
+            geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
+            geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+
+        }
+
+        var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } );
+
+        var line = new THREE.Line( geometry, material );
+        line.type = THREE.LinePieces;
+        scene.add( line );
+
+        
     }
 
     loadModels();
@@ -49,7 +86,7 @@
             texture.image = image;
             texture.needsUpdate = true;
 
-        });
+        }); 
 
         var loader = new THREE.OBJLoader(manager);
         loader.load( 'models/FoosballPlayer_new.obj', function(model){
@@ -58,14 +95,14 @@
                     child.material.map = texture;
                 }
             });
-
-            model.position.x = 0;
-            model.position.y = 0;
-            model.position.z = 0;
 
             leftStick = model;
 
-            scene.add(model);
+            leftStick.position.set(-400, 0, 0);
+            leftStick.scale.set(100,100,100);
+            
+            
+            scene.add(leftStick);
 
         });
 
@@ -77,44 +114,54 @@
                 }
             });
 
-            model.position.x = 4;
-            model.position.y = 0;
-            model.position.z = 0;
-
             rightStick = model;
 
-            scene.add( model );
+            rightStick.position.set(400, 0, 0);
+            rightStick.scale.set(100,100,100);
+
+            scene.add( rightStick );
 
         });
 
-        football = new THREE.Mesh(new THREE.SphereGeometry(0.2, 100, 100), new THREE.MeshNormalMaterial());
+        football = new THREE.Mesh(new THREE.SphereGeometry(10, 100, 100), new THREE.MeshNormalMaterial());
         football.overdraw = true;
+
+        football.position.set(0, 0, 0);
+
         scene.add(football);
     }
     
 
     function render() {
-        var timer = Date.now() * 0.005;
-
-        football.position.x = 1 * Math.cos(timer);
-        football.position.y = 1 * Math.sin(timer);
+        var timer = Date.now() * 0.05;
 
         
-
         renderer.render(scene, camera);
     };
 
     function update() {
+
+        /* keyboard stuff */
+
         keyboard.update();
 
         var moveDistance = 5 * clock.getDelta(); 
 
-        if(keyboard.pressed("up")) 
-            leftStick.translateZ( -0.1 );
+        if(keyboard.pressed("up")){
+            leftStick.translateZ( -10 );
+        }
 
-        if(keyboard.pressed("down")) 
-            leftStick.translateZ(  0.1 );
+        if(keyboard.pressed("down")){
+            leftStick.translateZ(  10 );
+        }
 
+        if(keyboard.pressed("left")){
+            leftStick.rotation.z += 0.3;
+        }
+        if(keyboard.pressed("right")){
+            leftStick.rotation.z -= 0.3;
+        }
+        
     }
 
     function animate() {
@@ -128,3 +175,26 @@
 
     animate();
 })();
+
+
+/*
+
+$.ajax({
+    type : 'get',
+    url : 'http://www.telize.com/geoip?callback=getgeoip',
+    success : function(data){
+        getPlayerDetails(data.country);
+    }
+});
+
+function getPlayerDetails(country){
+    $.ajax({
+        type : 'post',
+        url : 'php/index.php',
+        data : { 'country_name' : country },
+        success : function(data){
+            console.log(data);
+        }
+    });
+}
+*/
