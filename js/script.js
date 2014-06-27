ea20516ae4e2a68c9e8923b560e104fd8837d65e
Sangha Lee <sanghal@andrew.cmu.edu>
2014-06-27 15:00:43 -0400

    ug

--- js/script.js
+++ js/script.js
@@ -37,16 +37,21 @@ function init(){
     // test camera
     
     // camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 1, 2000)
-    // camera.position.set(-750, 350, 10);
+    // camera.position.set(1250, -900, 170);
     // camera.rotation.x = Math.PI / 2;
-    // camera.rotation.y = -Math.PI/2;
-    
+    // camera.rotation.y = Math.PI / 2;
 
+
+   
+    // working camera 
+    //camera = new THREE.OrthographicCamera( -aspectRatio * viewSize / 2, aspectRatio * viewSize / 2, viewSize / 2, -viewSize / 2, -2000, 2000 );
+
+    // hoping to make this work (currently using)
+    camera = new THREE.OrthographicCamera((window.innerWidth / - 2) - 100, (window.innerWidth / 2)+100, (window.innerHeight / 2) + 100, (window.innerHeight / - 2) - 100, -1000, 2000);
     
-    camera = new THREE.OrthographicCamera( -aspectRatio * viewSize / 2, aspectRatio * viewSize / 2, viewSize / 2, -viewSize / 2, -2000, 2000 );
-    camera.position.set(0, 0, 250);
-    camera.rotation.x = Math.PI / 2;
-    camera.lookAt(new THREE.Vector3(0, 50, 0));
+    camera.position.set(0, 0, 100);
+    //camera.rotation.x = Math.PI / 2;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
     
 
     renderer.setClearColor( 0x000000, 0 );
@@ -113,12 +118,12 @@ function loadModels(){
     
     */
 
-    leftStick = createPlayer("left-stick", -400, 0xAA3939);        
+    leftStick = createPlayer("left-stick", camera.left +300, 0xAA3939);        
     scene.add(leftStick);
     leftStick.children[2].name = "my stick";
     collidableMeshList.push(leftStick.children[2]);
 
-    rightStick = createPlayer("right-stick", 400, 0x4B5AB4);
+    rightStick = createPlayer("right-stick", camera.right -300, 0x4B5AB4);
     scene.add(rightStick);
     rightStick.children[2].name = "not my stick";
     collidableMeshList.push(rightStick.children[2]);
@@ -134,7 +139,7 @@ function loadModels(){
 
     goal1 =new THREE.Mesh(new THREE.CubeGeometry(30, 10, 150), new THREE.MeshNormalMaterial());
     goal1.overdraw = true;
-    goal1.position.set(-600, 0, 0);
+    goal1.position.set(camera.left +150, 0, 0);
     goal1.rotation.x = Math.PI / 2;
     goal1.name = "goal1";
     goal1.type = 0;
@@ -143,7 +148,7 @@ function loadModels(){
 
     goal2 = goal1.clone();
     goal2.overdraw = true;
-    goal2.position.set(600, 0, 0);
+    goal2.position.set(camera.right -150, 0, 0);
     goal2.rotation.x = Math.PI / 2;
     goal2.name = "goal2";
     goal2.type = 2;
@@ -152,31 +157,45 @@ function loadModels(){
 
     //adding invisible walls around
 
-    var wallGeometry = new THREE.CubeGeometry( 1250, 10, 200, 1, 1, 1 );
+    var wallGeometry = new THREE.CubeGeometry( 2000, 20, 400, 10, 10, 10 );
     var wallMaterial = new THREE.MeshBasicMaterial({ transparent : true, opacity: 0 });
-    var wall = new THREE.Mesh(wallGeometry, wallMaterial);
+    var wall = new THREE.Mesh(wallGeometry, wallMaterial); //bottom
 
-    var wall2 = wall.clone();
-    var wall3 = wall.clone();
-    var wall4 = wall.clone();
+    var wall2 = wall.clone();//right
+    var wall3 = wall.clone();//top
+    var wall4 = wall.clone();//left
 
-    wall.position.set(0, -350, 50);
-    wall2.position.set(0, 330, 50);
+    //wall.position.set(-20, -350, 50);
+    wall.position.set(-20, -(camera.top-100), 50);
 
-    wall3.position.set(650, -200, 50);
-    wall3.rotation.z = Math.PI / 2;
-    wall4.position.set(-650, -200, 50);
+    //wall2.position.set(950, -330, 150);
+    wall2.position.set(camera.right-100, -330, 150);
+    wall2.rotation.z = Math.PI/2;
+
+    //wall3.position.set(0, 330, 150);
+    wall3.position.set(0, camera.top-100, 150);
+
+    //wall4.position.set(-950, -200, 150);
+    wall4.position.set(camera.left+100, -200, 150);
     wall4.rotation.z = Math.PI / 2;
+
+    wall.type = 1;
+    wall.name = "wall";
+
+    wall2.type = 1;
+    wall2.name = "wall2";
+
+    wall3.type = 1;
+    wall3.name = "wall3";
+
+    wall4.type = 1;
+    wall4.name = "wall4";
 
     scene.add(wall);
     scene.add(wall2);
     scene.add(wall3);
     scene.add(wall4);
-
-    wall.type = 1;
-    wall2.type = 1;
-    wall3.type = 1;
-    wall4.type = 1;
+    
 
     collidableMeshList.push(wall);
     collidableMeshList.push(wall2);
@@ -186,12 +205,14 @@ function loadModels(){
 
     setTimeout(function  () {
         isGameOver = false;
-        timer(300);
+        timer(100000);
     }, 4000);
     
 
 
     animate();
+
+    
     
 }
 
@@ -259,10 +280,12 @@ function update() {
 
     ballSpeed += (3 - ballSpeed) * 0.005;
 
-    var rays = [
+var rays = [
+        new THREE.Vector3(0, -1, 0), // wall
+        new THREE.Vector3(1, 0, 0), // wall2
+        new THREE.Vector3(-1, 1, 0), // wall4
         new THREE.Vector3(0, 0, 1),
         new THREE.Vector3(1, 0, 1),
-        new THREE.Vector3(1, 0, 0),
         new THREE.Vector3(1, 0, -1),
         new THREE.Vector3(0, 0, -1),
         new THREE.Vector3(-1, 0, -1),
@@ -278,6 +301,7 @@ function update() {
         caster.set(football.position, rays[i]);
         collisions = caster.intersectObjects(collidableMeshList);
         if (collisions.length > 0 && collisions[0].distance <= distance) {
+            console.log(collisions[0].object.type);
             if( collisions[0].object.type == 0 ){
                 opponentScore++;
                 isGoalScored = true;
@@ -289,11 +313,24 @@ function update() {
                 resetBall("player");
                 isGoalScored = true;
                 scoreCard(myScore, opponentScore);
+            } else if (collisions[0].object.type == 1){
+                if(collisions[0].object.name == 'wall' ){
+                    ballDirY = -ballDirY;
+                }else if( collisions[0].object.name == 'wall2' ){
+                    ballDirX = -ballDirX;
+                }else if( collisions[0].object.name == 'wall3'){
+                    ballDirY = -ballDirY;
+                }else{
+                    ballDirX = -ballDirX;
+                }
+                ballSpeed =8;
+            }else{
+                if(collisions[0].object.name == "left-stick"){
+                    ballDirY = -ballDirY;
+                }else{
+                    ballDirX = -ballDirX;
+                }
             }
-
-            //console.log("collided " + collisions[0].object.name);
-            ballDirX = -ballDirX;
-            ballSpeed =8;
         }
     }
 }
@@ -333,38 +370,6 @@ function goalScored(winner) {
 
 function ballPhysics() {
 
-
-    if (football.position.x <= -600) {
-        if (football.position.z <= 75 && football.position.z >= -75){
-
-            //opponentScore++;
-            //resetBall("opponent");
-            //console.log('will update opponent score ' + opponentScore);
-            //scoreCard(myScore, opponentScore);
-            //console.log("I loose");
-            // document.getElementById("scores").innerHTML = score1 + "-" + score2;
-            // resetBall(2);
-            // matchScoreCheck();  
-        }
-        ballDirX = -ballDirX;
-    } else if (football.position.x >= 600) {
-        if (football.position.z <= 75 && football.position.z >= -75) {
-
-            //myScore++;
-            //resetBall("player");
-            //scoreCard(myScore, opponentScore);
-            // document.getElementById("scores").innerHTML = score1 + "-" + score2;
-            // resetBall(1);
-            // matchScoreCheck();  
-        }
-        ballDirX = -ballDirX;
-    } else if (football.position.y <= -400) {
-        ballDirY = -ballDirY;
-    }
-    // if ball goes off the bottom side (side of table)
-    else if (football.position.y >= 400) {
-        ballDirY = -ballDirY;
-    }
 
     // update ball position over time
     football.position.x += ballDirX * ballSpeed;
@@ -409,8 +414,8 @@ function opponentPaddleMovement(){
 
 function resetPositions () {
     football.position.set(0, 0, 0);
-    rightStick.position.set(400, 0, 80);
-    leftStick.position.set(-400, 0, 80);
+    leftStick.position.set(camera.left+300, 0, 80);
+    rightStick.position.set(camera.right-300, 0, 80);
 }
 
 function animate() {
